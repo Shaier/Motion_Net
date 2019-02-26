48654b14d43720f28d3561d521d78166eadc6f0e
Sagi Shaier <Sagishaier@gmail.com>
2019-02-26 10:43:05 -0800

    Update Pool.py

--- Pool.py
+++ Pool.py
@@ -2,10 +2,14 @@ def cov_forward(X,W,b, stride=1,padding=1):
     pass
 '''X: DxCxHxW dimension, input filter W: NFxCxHFxHW, and bias b: Fx1'''
 '''
-D is the number of input
-C is the number of image channel
-H is the height of image
-W is the width of the image
+D is the number of inputs --> number of images
+
+C is the number of image channel --> 3 RGB or 1 Grey
+H is the height of image --> *
+W is the width of the image --> *
+
+***** F is the number of frames????
+
 NF is the number of filter in the filter map W
 HF is the height of the filter, and finally
 HW is the width of the filter.
