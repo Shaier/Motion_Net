e124ce53c4706123bf747e08b52b1895a01030dc
Sagi Shaier <Sagishaier@gmail.com>
2019-02-27 16:04:33 -0800

    Update Pool.py

--- Pool.py
+++ Pool.py
@@ -248,6 +248,10 @@ def extract_labels(filename, num_images):
     return labels
 
 '''Step 2: Initialize parameters'''
+'''We first define methods to initialize both the filters for the convolutional layers and the weights for the dense layers.
+To make for a smoother training process, we initialize each filter with a mean of 0 and a standard deviation of 1.
+'''
+
 def initializeFilter(size, scale = 1.0):
     '''
     Initialize filter using a normal distribution with and a
@@ -261,3 +265,67 @@ def initializeWeight(size):
     Initialize weights with a random normal distribution
     '''
     return np.random.standard_normal(size=size) * 0.01
+
+
+'''Step 3: Define the backpropagation operations'''
+
+def convolutionBackward(dconv_prev, conv_in, filt, s):
+    '''
+    Backpropagation through a convolutional layer.
+    '''
+    (n_f, n_c, f, _) = filt.shape
+    (_, orig_dim, _) = conv_in.shape
+    ## initialize derivatives
+    dout = np.zeros(conv_in.shape)
+    dfilt = np.zeros(filt.shape)
+    dbias = np.zeros((n_f,1))
+    for curr_f in range(n_f):
+        # loop through all filters
+        curr_y = out_y = 0
+        while curr_y + f <= orig_dim:
+            curr_x = out_x = 0
+            while curr_x + f <= orig_dim:
+                # loss gradient of filter (used to update the filter)
+                dfilt[curr_f] += dconv_prev[curr_f, out_y, out_x] * conv_in[:, curr_y:curr_y+f, curr_x:curr_x+f]
+                # loss gradient of the input to the convolution operation (conv1 in the case of this network)
+                dout[:, curr_y:curr_y+f, curr_x:curr_x+f] += dconv_prev[curr_f, out_y, out_x] * filt[curr_f]
+                curr_x += s
+                out_x += 1
+            curr_y += s
+            out_y += 1
+        # loss gradient of the bias
+        dbias[curr_f] = np.sum(dconv_prev[curr_f])
+
+    return dout, dfilt, dbias
+
+def nanargmax(arr):
+    '''
+    return index of the largest non-nan value in the array. Output is an ordered pair tuple
+    '''
+    idx = np.nanargmax(arr)
+    idxs = np.unravel_index(idx, arr.shape)
+    return idxs
+
+def maxpoolBackward(dpool, orig, f, s):
+    '''
+    Backpropagation through a maxpooling layer. The gradients are passed through the indices of greatest value in the original maxpooling during the forward step.
+    '''
+    (n_c, orig_dim, _) = orig.shape
+
+    dout = np.zeros(orig.shape)
+
+    for curr_c in range(n_c):
+        curr_y = out_y = 0
+        while curr_y + f <= orig_dim:
+            curr_x = out_x = 0
+            while curr_x + f <= orig_dim:
+                # obtain index of largest value in input for current window
+                (a, b) = nanargmax(orig[curr_c, curr_y:curr_y+f, curr_x:curr_x+f])
+                dout[curr_c, curr_y+a, curr_x+b] = dpool[curr_c, out_y, out_x]
+
+                curr_x += s
+                out_x += 1
+            curr_y += s
+            out_y += 1
+
+    return dout
