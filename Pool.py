644c8ab2538accf9ac3a4d3143b5c0158434f38d
Sagi Shaier <Sagishaier@gmail.com>
2019-03-04 18:31:51 -0800

    Update Pool.py

--- Pool.py
+++ Pool.py
@@ -647,3 +647,57 @@ label = decode_predictions(yhat)
 # print the classification
 #print('%s (%.2f%%)' % (label[1], label[2]*100))
 label
+
+
+
+#Extracting features from images
+#The image features are a 1-dimensional 4,096 element vector.
+#At the end of the run, you will have the extracted features stored in ‘features.pkl‘ for later use. This file will be about 127 Megabytes in size.
+
+from os import listdir
+from pickle import dump
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+
+'''extract features from each photo in the directory'''
+def extract_features(directory):
+	# load the model
+	model = VGG16()
+	# re-structure the model
+	model.layers.pop()
+	model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+	# summarize
+	print(model.summary())
+	# extract features from each photo
+	features = dict()
+	for name in listdir(directory):
+		# load an image from file
+		filename = directory + '/' + name
+		image = load_img(filename, target_size=(224, 224))
+		# convert the image pixels to a numpy array
+		image = img_to_array(image)
+		# reshape data for the model
+		image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+		# prepare the image for the VGG model
+		image = preprocess_input(image)
+		# get features
+		feature = model.predict(image, verbose=0)
+		# get image id
+		image_id = name.split('.')[0]
+		# store feature
+		features[image_id] = feature
+		print('>%s' % name)
+	return features
+
+#call this function to prepare the photo data for testing our models, then save the resulting dictionary to a file named ‘features.pkl‘.
+
+
+# extract features from all images
+directory = 'cell_images/Uninfected'
+features = extract_features(directory)
+print('Extracted Features: %d' % len(features))
+# save to file
+dump(features, open('features.pkl', 'wb'))
