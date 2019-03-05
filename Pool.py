b93dab2d8468741beac3e54ce97dc5cc5e7e3a45
Sagi Shaier <Sagishaier@gmail.com>
2019-03-05 08:29:57 -0800

    Update Pool.py

--- Pool.py
+++ Pool.py
@@ -724,6 +724,41 @@ for x in data:
   plt.show()
 
 ###########################################
+''''Loading VGG'''
+from keras.applications.vgg16 import VGG16
+model = VGG16(weights='imagenet', include_top=True)
+
+'''extract features from each photo in the directory'''
+def extract_features(directory):
+	# load the model
+	model = VGG16()
+	# re-structure the model
+	model.layers.pop()
+	model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+	# summarize
+	print(model.summary())
+	# extract features from each photo
+	features = dict()
+	for name in listdir(directory):
+		# load an image from file
+		filename = directory + '/' + name
+		image = load_img(filename, target_size=(224, 224))
+		# convert the image pixels to a numpy array
+		image = img_to_array(image)
+		# reshape data for the model
+		image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+		# prepare the image for the VGG model
+		image = preprocess_input(image)
+		# get features
+		feature = model.predict(image, verbose=0)
+		# get image id
+		image_id = name.split('.')[0]
+		# store feature
+		features[image_id] = feature
+		print('>%s' % name)
+	return features
+
+
 
 matrix=[[x1],[x2],[x3],[x4],[x5],[x6],[x7],[x8],[x9],[x10]]
 
