5619c20925c22de684a9c0c3ab74357d1f9df152
Sagi Shaier <Sagishaier@gmail.com>
2019-03-05 08:04:47 -0800

    Update Pool.py

--- Pool.py
+++ Pool.py
@@ -723,4 +723,6 @@ for x in data:
   plt.imshow(data[x].reshape((64, 64)), cmap=cm.Greys_r)
   plt.show()
 
-  
+###########################################
+
+matrix=[[x1],[x2],[x3],[x4],[x5],[x6],[x7],[x8],[x9],[x10]]
