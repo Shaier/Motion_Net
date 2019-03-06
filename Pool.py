275ce474db97cb97c9f371ee21a3a8a8084058b0
Sagi Shaier <Sagishaier@gmail.com>
2019-03-06 08:57:42 -0800

    Update Pool.py

--- Pool.py
+++ Pool.py
@@ -891,3 +891,40 @@ hidden = Dense(2)(visible)
 #   from your created layers. It requires that you only specify the
 #   input and output layers. For example:
 model = Model(inputs=visible, outputs=hidden)
+
+
+'''new architecture'''
+# Multiple Inputs
+from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+# first input model
+visible1 = Input(shape=(64,64,1))
+conv11 = Conv2D(32, kernel_size=4, activation='relu')(visible1)
+pool11 = MaxPooling2D(pool_size=(2, 2))(conv11)
+conv12 = Conv2D(16, kernel_size=4, activation='relu')(pool11)
+pool12 = MaxPooling2D(pool_size=(2, 2))(conv12)
+flat1 = Flatten()(pool12)
+# second input model
+visible2 = Input(shape=(32,32,3))
+conv21 = Conv2D(32, kernel_size=4, activation='relu')(visible2)
+pool21 = MaxPooling2D(pool_size=(2, 2))(conv21)
+conv22 = Conv2D(16, kernel_size=4, activation='relu')(pool21)
+pool22 = MaxPooling2D(pool_size=(2, 2))(conv22)
+flat2 = Flatten()(pool22)
+# merge input models
+merge = concatenate([flat1, flat2])
+# interpretation model
+hidden1 = Dense(10, activation='relu')(merge)
+hidden2 = Dense(10, activation='relu')(hidden1)
+output = Dense(1, activation='sigmoid')(hidden2)
+model = Model(inputs=[visible1, visible2], outputs=output)
+# summarize layers
+print(model.summary())
+# plot graph
+plot_model(model, to_file='multiple_inputs.png')
