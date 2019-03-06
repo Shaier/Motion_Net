16851688f883f54d73be7e3d0bf1e64f1308faff
Sagi Shaier <Sagishaier@gmail.com>
2019-03-06 14:59:50 -0800

    Update Pool.py

--- Pool.py
+++ Pool.py
@@ -991,5 +991,11 @@ train1=[frame1,frame2...frame_N-9]
 train2=[frame2,frame3...frame_N-8]
 ...
 train9=[frame9,frame10...frame_N-1] #Note that we go up to N-1 because we need the last frame to be an output
+*** Look at them from top to bottom: train1[0],train2[0]...train9[0] is sequence 1.
+and
+output1=[frame2,frame3...frame_N-8]
+output2=[frame3,frame4...frame_N-7]
+...
+output9=[frame10,frame11...frame_N] #Note that we go up to N because the last frame is an output
 
 '''
