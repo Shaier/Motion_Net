cbb638b99b7dd13c0df28dc7ccc37750914fa5f3
Sagi Shaier <Sagishaier@gmail.com>
2019-03-07 13:36:35 -0800

    Update Model1.py

--- Model1.py
+++ Model1.py
@@ -203,12 +203,12 @@ from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
 
-model = VGG16(weights='imagenet', include_top=False)
+model3 = VGG16(weights='imagenet', include_top=False)
 
-img_path = 'elephant.jpg'
+img_path = 'video/img0001.jpg'
 img = image.load_img(img_path, target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
-features = model.predict(x)
+features = model3.predict(x)
