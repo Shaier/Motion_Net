7479364ff435e370a674b8ab7dbd45c03c116e5b
Sagi Shaier <Sagishaier@gmail.com>
2019-03-10 12:01:23 -0700

    Update Model1.py

--- Model1.py
+++ Model1.py
@@ -310,7 +310,8 @@ use l2 and l1 regularization
 perhaps just get the edges (instead of the last layer of VGG) of a 96x96 say images
 then use ssim loss
 perhaps use 96x96, get edges, run model1, train it. then compare 9 images using MSSIM loss function
-
+if SSIM doesnt work:
+I still need to compare the first image to the next
 '''
 def mse(imageA, imageB):
 	# the 'Mean Squared Error' between the two images is the
