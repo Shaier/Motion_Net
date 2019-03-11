b98b19bfd36b73eaa16ee87d7189a2d9110636f1
Sagi Shaier <Sagishaier@gmail.com>
2019-03-10 19:30:03 -0700

    Update Model1.py

--- Model1.py
+++ Model1.py
@@ -118,54 +118,6 @@ output2=[frame3,frame4...frame_N-7]
 output9=[frame10,frame11...frame_N] #Note that we go up to N because the last frame is an output
 
 '''
-'''
-Extract features with VGG16'''
-vgg = VGG16(weights='imagenet', include_top=False)
-
-#img_path = 'video/img0001.jpg'
-'''
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-features = vgg.predict(x)
-features.shape
-'''
-vgg = VGG16(weights='imagenet', include_top=False)
-
-#folder with images
-image_dir=os.listdir('salsa_dance_images')
-img_path = 'salsa_dance_images/'
-
-#create a list to hold the array of pixels of each image
-images_array=[]
-count=0
-#place the pixels for each image in the list
-for image_name in image_dir:
-  try:
-    location=str(img_path +str(image_name))
-    img = image.load_img(location, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    features = vgg.predict(x)
-    images_array.append(features)
-    if count%1000==0:
-      print(count)
-    count+=1
-  except:
-    pass
-
-
-#Save the list to a file
-
-import pickle
-
-#with open('images_array', 'wb') as fp:
-#    pickle.dump(images_array, fp)
-
-with open ('images_array', 'rb') as fp:
-    images_array = pickle.load(fp)
 
 #Load the data
 
@@ -308,7 +260,7 @@ from skimage.measure import structural_similarity as ssim
 
 use l2 and l1 regularization
 perhaps just get the edges (instead of the last layer of VGG) of a 96x96 say images
-then use ssim loss to compare 
+then use ssim loss to compare
 perhaps use 96x96, get edges, run model1, train it. then compare 9 images using MSSIM loss function
 if SSIM doesnt work:
 I still need to compare the first image to the next
