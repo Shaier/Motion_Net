6497c55f57e5395cef62443b01bd9ebe9e0b0e82
Sagi Shaier <Sagishaier@gmail.com>
2019-03-10 19:09:54 -0700

    Update Extract_from_VGG.py

--- Extract_from_VGG.py
+++ Extract_from_VGG.py
@@ -20,3 +20,25 @@ x = preprocess_input(x)
 # get the features
 block4_pool_features = model.predict(x)
 block4_pool_features.shape
+
+
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+from keras.models import Sequential
+from keras.layers import *
+import pylab
+model = VGG16(weights='imagenet', include_top=False)
+
+img_path = 'salsa_dance_images/bandicam 2019-03-09 08-51-20-717frame45.jpg'
+img = image.load_img(img_path)
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+features = model.predict(x)
+pic=features[0,:,:,21]
+pylab.imshow(pic)
+pylab.gray()
+pylab.show()
