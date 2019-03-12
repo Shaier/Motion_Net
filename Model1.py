d3522c9fa1c78a603af0aa985a4ded6d1eebb8ea
Sagi Shaier <Sagishaier@gmail.com>
2019-03-11 20:26:22 -0700

    Update Model1.py

--- Model1.py
+++ Model1.py
@@ -270,7 +270,7 @@ also need to convert to 96x96?
 
 for tomorrow:
 check loss before and after each one
-normalize the data--
+normalize the data
 Change batch size---
 
 Is the network size is too small / large?
