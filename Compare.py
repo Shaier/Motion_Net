14540eb7e9f5243199346008dc182e647bb95496
Sagi Shaier <Sagishaier@gmail.com>
2019-03-22 08:50:50 -0700

    Update Compare.py

--- Compare.py
+++ Compare.py
@@ -7,8 +7,7 @@
 3.compare output 1 to input 2, output 2 to input 3... up to output 8 to input 9. (we dont use output 9)
 
 -->MSE is dead simple to implement — but when using it for similarity, we can run into problems. The main one being that large distances
-between pixel intensities do not necessarily mean the contents of the images are dramatically different. I’ll provide some proof for that
-statement later in this post, but in the meantime, take my word for it.
+between pixel intensities do not necessarily mean the contents of the images are dramatically different.
 It’s important to note that a value of 0 for MSE indicates perfect similarity. A value greater than one implies less similarity and will continue to grow as the average difference between pixel intensities increases as well.
 
 In order to remedy some of the issues associated with MSE for image comparison, we have the Structural Similarity Index, developed by Wang et al.:
