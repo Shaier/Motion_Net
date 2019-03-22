9b2e196bb2f962c161ea08b5f2710a044f405f52
Sagi Shaier <Sagishaier@gmail.com>
2019-03-22 14:55:52 -0700

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -5,7 +5,6 @@ import os
 
 os.chdir('C:\\Users\\sagi\\Desktop\\test')
 os.getcwd()
-FrameCapture('C:\\Users\\sagi\\Desktop\\test\\dance.mp4','dance','C:\\Users\\sagi\\Desktop\\test')
 # Function to extract frames
 def FrameCapture(video_path,video_name,images_path):
 
@@ -22,24 +21,23 @@ def FrameCapture(video_path,video_name,images_path):
         # vidObj object calls read
         # function extract frames
         success, image = vidObj.read()
-        if count % 10 ==0: #10 frames per sec 
+        if count % 10 ==0: #10 frames per sec
             # Saves the frames with frame-count
             #cv2.imwrite(images_path+'\\'+video_name+"frame%d.jpg" % count, image)
             cv2.imwrite(images_path+'\\'+"%d.jpg" % count, image)
             count += 1
 
+#Calling the function 
+FrameCapture('C:\\Users\\sagi\\Desktop\\test\\dance.mp4','dance','C:\\Users\\sagi\\Desktop\\test')
 
 
 videos_path='salsa_dance_videos/'
 vid_dir=os.listdir(videos_path)
 images_path='salsa_dance_images/'
 
+#Convert multiple videos
 for video in vid_dir:
   video=video.split('.')
   video_name=str(video[0])
   video_path=videos_path+video_name +'.mp4'
-  #print(video_path)
   FrameCapture(video_path,video_name,images_path)
-
-# Calling the function
-#FrameCapture("salsa_dance_videos/bandicam 2019-03-09 08-51-20-717.mp4")
