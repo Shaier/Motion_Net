f103ca372077510cdc1bfdcf0104afcf7614a168
Sagi Shaier <Sagishaier@gmail.com>
2019-03-26 11:19:07 -0700

    update

--- Compare.py
+++ Compare.py
@@ -1,42 +1,26 @@
 '''Comparing images'''
 
 '''
-3/10- now when the model is running i need a way to compare testing images
-2.Inserting 9 images, getting an output of 9 images
-
-3.compare output 1 to input 2, output 2 to input 3... up to output 8 to input 9. (we dont use output 9)
+Inserting 9 images, getting an output of 9 images
+compare output 1 to input 2, output 2 to input 3... up to output 8 to input 9. (we dont use output 9)
 
 -->MSE is dead simple to implement — but when using it for similarity, we can run into problems. The main one being that large distances
 between pixel intensities do not necessarily mean the contents of the images are dramatically different.
-It’s important to note that a value of 0 for MSE indicates perfect similarity. A value greater than one implies less similarity and will continue to grow as the average difference between pixel intensities increases as well.
-
+It’s important to note that a value of 0 for MSE indicates perfect similarity. A value greater than one implies less similarity and
+will continue to grow as the average difference between pixel intensities increases as well.
 In order to remedy some of the issues associated with MSE for image comparison, we have the Structural Similarity Index, developed by Wang et al.:
 
-SSIM attempts to model the perceived change in the structural information of the image, whereas MSE is actually estimating the perceived errors. There is a subtle difference between the two, but the results are dramatic.
-Furthermore, the equation in Equation 2 is used to compare two windows (i.e. small sub-samples) rather than the entire image as in MSE. Doing this leads to a more robust approach that is able to account for changes in the structure of the image, rather than just the perceived change.
-The parameters to Equation 2 include the (x, y) location of the N x N window in each image, the mean of the pixel intensities in the x and y direction, the variance of intensities in the x and y direction, along with the covariance.
+SSIM attempts to model the perceived change in the structural information of the image, whereas MSE is actually estimating the perceived errors.
+There is a subtle difference between the two, but the results are dramatic.
+Furthermore, the equation in Equation 2 is used to compare two windows (i.e. small sub-samples) rather than the entire image as in MSE.
+Doing this leads to a more robust approach that is able to account for changes in the structure of the image, rather than just the perceived change.
 Unlike MSE, the SSIM value can vary between -1 and 1, where 1 indicates perfect similarity.
 
-then use ssim loss to compare
-perhaps use 96x96, get edges, run model1, train it. then compare 9 images using MSSIM loss function
-
-
-3/22
-take 9 diff images (make sure you have 2 of each - frame 1 and frame 2)
-compare the similarities
-
-get a sequence of another MOVEMENT
-predict
-get the SSIM
-add them all
-compare that score to the SSIM from a correct sequence
 
 *****I need to use several inputs/outputs because one frame might look the same but the others not and at the end Ill get a bad score
 if its a different MOVEMENT
 '''
 
-#Not surpassingly, the original image is identical to itself, with a value of 0.0 for MSE and 1.0 for SSIM. Remember, as
-#the MSE increases the images are less similar, as opposed to the SSIM where smaller values indicate less similarity.
 
 
 # import the necessary packages
--- Extract_features_using_VGG16.py
+++ Extract_features_using_VGG16.py
@@ -1,4 +1,6 @@
 '''Extract features with VGG16'''
+from keras.applications.vgg16 import VGG16
+
 vgg = VGG16(weights='imagenet', include_top=False)
 
 #folder with images
